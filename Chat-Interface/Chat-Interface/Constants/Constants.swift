97fc3d87c58c9735c89d97e245c0cf75d670ec30
neo-Fernando-Arenas <fernando.arenas@neofinancial.com>
2025-04-16 21:01:02 -0600

    Create Constants Files
    - AppIcon
    - Strings
    - Sizing
    - CornerRadius
    - Spacing
    - FontSize

---
+++ Chat-Interface/Chat-Interface/Constants/Constants.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+enum Sizing {
+    /// 4pt
+    static let extraExtraExtraSmall: CGFloat = 4
+    
+    /// 8pt
+    static let extraExtraSmall: CGFloat = 8
+    
+    /// 16pt
+    static let extraSmall: CGFloat = 16
+    
+    /// 20pt
+    static let small: CGFloat = 20
+    
+    /// 24pt
+    static let medium: CGFloat = 24
+    
+    /// 40pt
+    static let extraLarge: CGFloat = 40
+    
+    /// 112pt
+    static let photo: CGFloat = 60
+}
+
+enum CornerRadius {
+    /// 10pt
+    static let small: CGFloat = 10
+    
+    /// 24pt
+    static let medium: CGFloat = 20
+}
+
+enum Spacing {
+    /// 4pt
+    static let extraExtraExtraSmall: CGFloat = 4
+    
+    /// 8pt
+    static let extraExtraSmall: CGFloat = 8
+    
+    /// 16pt
+    static let extraSmall: CGFloat = 16
+}
+
+enum FontSize {
+    /// 14pt
+    static let small: CGFloat = 14
+    
+    /// 16pt
+    static let medium: CGFloat = 16
+    
+    /// 18pt
+    static let large: CGFloat = 18
+}
---
+++ Chat-Interface/Chat-Interface/Constants/L10n.swift
@@ -0,0 +1,19 @@
+enum L10n {
+    enum ChatBottomSheet {
+        static let textField = "Text field"
+        static let textFieldPlaceholder = "Start typing..."
+        static let photosPickerLabel = "Add Picture"
+        static let photosAccessibilityLabel = "Open photo picker"
+        static let removePictureButton = "Remove selected picture"
+        static let expandTextEditorLabel = "Expand Text Editor"
+        static let expandTextEditorAccessibility = "Expand text input field"
+        static let sendMessageLabel = "Send"
+        static let sendMessageAccessibility = "Send message"
+        static let chipGatherResources = "Tell me how to gather resources"
+        static let chipTakePicturePlant = "Take a picture of this plant"
+        static let chipBoatMaterial = "What is this boat made of"
+        static let chipBeatMonster = "Tell me how to beat this monster"
+        static let chipBeatBoss = "Tell me how to beat this boss"
+        static let compressTextEditorLabel = "Compress Text Editor"
+    }
+}
